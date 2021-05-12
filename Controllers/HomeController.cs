f5dcde6118e8b8da544b29c4284cadef87083abd
jgrissom <jgrissom91@gmail.com>
2021-05-12 11:11:25 -0500

    Display Teams - Create  Home/Team controller method - Designate the list of Teams as the view’s model - Add Views/Home/Team.cshml - iterate thru the model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,16 @@
+using System.Data.Common;
 using System;
 using Microsoft.AspNetCore.Mvc;
+using MilkingPigeons.Models;
+using System.Linq;
 
 namespace Northwind.Controllers
 {
     public class HomeController : Controller
     {
+        private DataContext _dataContext;
+        public HomeController(DataContext db) => _dataContext = db;
         public ActionResult Index() => View();
+        public ActionResult Team() => View(_dataContext.Teams.OrderBy(t => t.Name));
     }
 }
