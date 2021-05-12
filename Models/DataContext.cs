dbdf98fa6710725feca59b3cad4648a96035ac59
jgrissom <jgrissom91@gmail.com>
2021-05-12 10:47:16 -0500

    Install Entity Framework / Create Data Model - dotnet add package Microsoft.EntityFrameworkCore.SqlServer -v 5.0.6 - Define Team - Define DataContext

---
+++ Models/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MilkingPigeons.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Team> Teams { get; set; }
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,9 @@
+namespace MilkingPigeons.Models
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string Name { get; set; }
+        public int Pin { get; set; }
+    }
+}
