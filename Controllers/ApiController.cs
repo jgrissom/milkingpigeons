636d91d347796f08ef026bd86bf508c0d2dc7f85
jgrissom <jgrissom91@gmail.com>
2021-05-13 00:07:17 -0500

    populate categories

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -11,9 +11,9 @@ namespace MilkingPigeons.Controllers
         private DataContext _dataContext;
         public APIController(DataContext db) => _dataContext = db;
 
-        // returns all categories
+        // returns all categories (with at least 1 chalenge)
         [HttpGet, Route("api/category")]
-        public IEnumerable<CategoryJson> GetCategories() => _dataContext.Categories.Select(c => new CategoryJson{Id = c.CategoryId, Name = c.Name}).OrderBy(c => c.Name);
+        public IEnumerable<CategoryJson> GetCategories() => _dataContext.Categories.Where(c => c.Challenges.Count > 0).Select(c => new CategoryJson{Id = c.CategoryId, Name = c.Name}).OrderBy(c => c.Name);
         // returns all challenges based on Active T/F
         [HttpGet, Route("api/challenge/active/{active}")]
         public IEnumerable<ChallengeJson> GetActiveChallenges(bool active) => _dataContext.Challenges.Where(c => c.Active == active).Select(c => new ChallengeJson{Id = c.ChallengeId, Name = c.Name, CatId = c.CategoryId}).OrderBy(c => c.Name);
